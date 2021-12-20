da8253c02497bd94474721a9bed26621e17e1cdd
dev-arminder <mail.to.arminder.singh@gmail.com>
2021-12-20 11:44:32 +0530

    Setting up pug :) views

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // Backend File for Express
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Setting Up Template Engine
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
-  res.send("Hello You are on Home Page");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
