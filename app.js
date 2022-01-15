3935ead9ef53bb490cd2a3519e36fa2a10ea2042
dev-arminder <mail.to.arminder.singh@gmail.com>
2022-01-16 01:36:36 +0530

    Implementing SCSS Arcitecture and setting up base files and folders

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(logger("dev"));
 
 app.use(methodOverride());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // For Parsing Incoming request
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
