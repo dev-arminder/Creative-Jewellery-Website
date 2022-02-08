83da672802f51630f632972a38b50f3544f36dcb
dev-arminder <mail.to.arminder.singh@gmail.com>
2022-02-09 01:25:34 +0530

    Fixing Some Bugs :BUGS-)

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -24,10 +24,13 @@ class Navigation extends Component {
         duration: 1.5
       });
       GSAP.to(this.elements.items[0], {
-        autoAlpha: 0
+        autoAlpha: 0,
+        duration: 0.75
       });
       GSAP.to(this.elements.items[1], {
-        autoAlpha: 1
+        autoAlpha: 1,
+        delay: 0.75,
+        duration: 0.75
       });
     } else {
       GSAP.to(this.element, {
@@ -35,7 +38,9 @@ class Navigation extends Component {
         duration: 1.5
       });
       GSAP.to(this.elements.items[0], {
-        autoAlpha: 1
+        autoAlpha: 1,
+        delay: 0.75,
+        duration: 0.75
       });
       GSAP.to(this.elements.items[1], {
         autoAlpha: 0
